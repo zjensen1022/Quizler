fda5b9720c72a17c0c3f6f4efc546036a308486a
Zac <zjensen1022@gmail.com>
2021-11-10 20:24:01 -0700

    Add Card class.

---
+++ app/src/main/java/com/example/quizler/Card.java
@@ -0,0 +1,15 @@
+package com.example.quizler;
+
+public class Card {
+
+    String frontText;
+    String backText;
+
+    public String getFrontText() {
+        return frontText;
+    }
+
+    public String getBackText() {
+        return backText;
+    }
+}
