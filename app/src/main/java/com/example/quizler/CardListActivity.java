121fa2d815348ae53054a4a6de9372498c4c56dc
Zac <zjensen1022@gmail.com>
2021-11-19 12:13:28 -0700

    Remove unused imports and variables, extract string resources

--- app/src/main/java/com/example/quizler/CardListActivity.java
+++ app/src/main/java/com/example/quizler/CardListActivity.java
@@ -4,8 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,17 +12,12 @@ import com.google.android.flexbox.FlexboxLayout;
 
 public class CardListActivity extends AppCompatActivity {
 
-    public static final String TAG = "CardListActivity";
-    private AppDatabase db;
     // Handles whether or not deck was edited
     ActivityResultLauncher<Intent> editDeckActivityResultLauncher = registerForActivityResult(
             new ActivityResultContracts.StartActivityForResult(),
-            new ActivityResultCallback<ActivityResult>() {
-                @Override
-                public void onActivityResult(ActivityResult result) {
-                    if (result.getResultCode() == RESULT_OK) {
-                        finish();
-                    }
+            result -> {
+                if (result.getResultCode() == RESULT_OK) {
+                    finish();
                 }
             }
     );
@@ -40,15 +33,15 @@ public class CardListActivity extends AppCompatActivity {
     private void createBottomBar() {
         FlexboxLayout bottomBar = findViewById(R.id.cardListBottomBarRef);
         CustomButton editDeck = new CustomButton(bottomBar.getContext());
-        editDeck.button.setText("Edit Deck");
+        editDeck.button.setText(R.string.edit_deck);
         editDeck.button.setOnClickListener(this::editDeck);
         bottomBar.addView(editDeck);
         CustomButton reviewButton = new CustomButton(bottomBar.getContext());
-        reviewButton.button.setText("Review");
+        reviewButton.button.setText(R.string.review);
         reviewButton.button.setOnClickListener(this::reviewDeck);
         bottomBar.addView(reviewButton);
         CustomButton addCardButton = new CustomButton(bottomBar.getContext());
-        addCardButton.button.setText("Add Card");
+        addCardButton.button.setText(R.string.add_card);
         addCardButton.button.setOnClickListener(this::addCard);
         bottomBar.addView(addCardButton);
     }
